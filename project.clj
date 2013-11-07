fb0836f561a913e568631c5b9bfa1302a0b776af
Rahkola Jani P <jprahkol@da3-cs-dk110-10.helsinki.fi>
2013-11-07 20:12:59 +0200

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject iloveponies.tests/training-day "0.1.0-SNAPSHOT"
+  :description "tests for training-day"
+  :url "http://github.com/iloveponies/training-day"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [midje "1.5.1"]
+                 [midje-grader "0.1.0-SNAPSHOT"]])
